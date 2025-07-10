a39b3eeff57d77a9e96f341db0072e7ca75cef28
carter <carter.appleton@gmail.com>
2025-07-10 15:48:46 -0700

    export token types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+export type { ClankerTokenV3 } from './config/clankerTokenV3.js';
+export type { ClankerTokenV4 } from './config/clankerTokenV4.js';
 export * from './constants.js';
 export { AirdropExtension } from './extensions/index.js';
 export * from './services/vanityAddress.js';
